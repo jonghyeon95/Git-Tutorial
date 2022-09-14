03e54cf1f7393156d943df868406fa4a02e4480f
Max Sim <Max.Sim@obigo.com>
2022-09-14 11:17:22 +0900

    컨트롤러 하나 등록

---
+++ demo/src/main/java/git/test/demo/Controller.java
@@ -0,0 +1,16 @@
+package git.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String main(){
+
+
+        return "main";
+    }
+
+}
