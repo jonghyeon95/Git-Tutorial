15e470ef332242e4601e4671c5cd667d02703484
Max Sim <Max.Sim@obigo.com>
2022-10-06 14:14:01 +0900

    프로젝트명 수정
    (cherry picked from commit 15127db1eacd3a340ccadfe7059531c3ef9c9cfe)

---
+++ gitTest/src/main/java/git/test/demo/Controller.java
@@ -0,0 +1,42 @@
+package git.test.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String main(){
+
+        System.out.println("Controller.main");
+        System.out.println("main이다");
+
+        return "main";
+    }
+
+    @GetMapping("/dev")
+    public String dev(){
+        return "dev";
+    }
+
+    @GetMapping("/dev2")
+    public String dev2(){
+        return "dev";
+    }
+
+    @GetMapping("/dev3")
+    public String dev3(){
+        return "dev";
+    }
+
+    @GetMapping("/hotfix")
+    public String hotfix(){
+        return "aaa";
+    }
+
+    @GetMapping("/def4")
+    public String def4(){
+        return "";
+    }
+}
